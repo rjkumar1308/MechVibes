69dd3f5ad947482973583c793deb7d18e297b5b2
rjkumar1308 <rjkumar1308@gmail.com>
2021-07-10 20:12:13 +0530

    added right click menu for taskbar icon

--- custTray.py
+++ custTray.py
@@ -3,6 +3,9 @@ import wx
 
 class CustomTaskBarIcon(wx.adv.TaskBarIcon ):
     
+    TBMENU_RESTORE = wx.NewId()
+    TBMENU_CLOSE   = wx.NewId()
+
     def __init__(self, frame):
         wx.adv.TaskBarIcon.__init__(self)
         self.frame = frame
@@ -14,6 +17,9 @@ class CustomTaskBarIcon(wx.adv.TaskBarIcon ):
         
         self.SetIcon(self.icon, "Restore")
         self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.OnTaskBarLeftClick)
+        self.Bind(wx.adv.EVT_TASKBAR_RIGHT_DOWN, self.OnTaskBarRightClick)
+        self.Bind(wx.EVT_MENU, self.OnTaskBarLeftClick, id=self.TBMENU_RESTORE)   
+        self.Bind(wx.EVT_MENU, self.OnTaskBarClose, id=self.TBMENU_CLOSE) 
  
     def OnTaskBarActivate(self, evt):
         pass
@@ -24,3 +30,12 @@ class CustomTaskBarIcon(wx.adv.TaskBarIcon ):
     def OnTaskBarLeftClick(self, evt):
         self.frame.Show()
         self.frame.Restore()
+
+    def CreatePopupMenu(self):
+        self.menu = wx.Menu()
+        self.menu.Append(self.TBMENU_RESTORE, "Restore")
+        self.menu.Append(self.TBMENU_CLOSE, "Close")
+        return self.menu
+
+    def OnTaskBarRightClick(self, evt):
+        self.CreatePopupMenu()
